d015c81360fc7481d94a84c719b6a849602fccc6
asmsuechan <suenagaryoutaabc@gmail.com>
2019-05-04 18:19:43 +0900

    Change to install empy

--- setup.sh
+++ setup.sh
@@ -60,5 +60,6 @@ git clone https://github.com/morioka-lab/ros
 cp -rf ros/* ~/my_workspace/src/
 sed -i -e "s/<USERNAME>/${USER}/g" ~/my_workspace/src/icart_navigation/launch/map.launch
 echo "source ~/my_workspace/devel/setup.bash" >> ~/.bashrc
+pip install empy
 printf "${G}Success to create new project in ~/my_workspace!${N}\n\n\n\n"
 printf "${B}ROSのセットアップが完了しました。~/my_workspaceにワークスペースが作成されました。${N}\n\n\n\n"
